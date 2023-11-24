a23affc3bf50af0fc8f66621682a9228de5afaeb
Hieu Dan <hieu_danminh@live.com>
2023-11-24 16:01:24 +0100

    Changes

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+function add7(num) {
+    return num + 7;
+}
+
+console.log(add7(1));
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(2,2));
+
+function name(params) {
+    
+}
+
+function capitalize(str) {
+    return str[0].toUpperCase();
+}
+
+console.log(capitalize("eating"));
+*/
+
+
+function lastLetter(string) {
+   
+   let LastLetter = string.charAt(string.length - 1);
+
+    return LastLetter;
+}
+
+
+console.log(lastLetter("FOODTRUCK"));
